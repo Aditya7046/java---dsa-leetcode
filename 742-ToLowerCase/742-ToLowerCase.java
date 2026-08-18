9f03f3e5d7f8453eee37870f9491b9eacaf1be1b
Aditya7046 <126277662+Aditya7046@users.noreply.github.com>
2026-08-18 09:18:51 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,10 @@
+// Last updated: 8/18/2026, 9:18:53 AM
+class Solution {
+    public String toLowerCase(String s) {
+        // return toUpperCase(s);
+        String lower = s.toLowerCase();
+        return lower;
+        
+
+    }
+}
