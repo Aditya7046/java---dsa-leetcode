66930c5226ddd58ee21614879d7581040c733275
Aditya7046 <126277662+Aditya7046@users.noreply.github.com>
2026-08-18 09:18:46 +0530

    Create 1205-DefangingAnIpAddress

---
+++ 1205-DefangingAnIpAddress/1205-DefangingAnIpAddress.java
@@ -0,0 +1,9 @@
+// Last updated: 8/18/2026, 9:18:47 AM
+class Solution {
+    public String defangIPaddr(String address) {
+        // String adres = address.replace("." , "[.]");
+        // System.out.print(adres);
+        return address.replace("." , "[.]");
+        
+    }
+}
